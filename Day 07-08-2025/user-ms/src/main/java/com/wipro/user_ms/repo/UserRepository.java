a143ea92cd96ac1b006dec2b0331babb8aa2cd08
VanukuriChaitanya29 <vanukurichaitanya@gmail.com>
2025-08-10 17:18:56 +0530

    Delete Day 07-08-2025 directory

--- Day 07-08-2025/user-ms/src/main/java/com/wipro/user_ms/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.user_ms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.user_ms.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
