fff872f3bf19eb77c2a714c89fb84d5eb30a9be6
VanukuriChaitanya29 <vanukurichaitanya@gmail.com>
2025-08-10 21:55:12 +0530

    Add files via upload

---
+++ Day 09-08-2025/Exercise-2 and Exercise-3/subjectproducer/src/main/java/com/wipro/subjectproducer/config/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package com.wipro.subjectproducer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.wipro.subjectproducer.entity.Subject;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, Subject> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Subject> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
